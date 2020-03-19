be2031e4105acc6da8003d0d223129f4743e619e
Franco Vuoto <vuoto@eng.it>
2020-03-19 18:18:33 +0100

    19

---
+++ precache-manifest.cd64c393a01e2348eca3c34bb500e9d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46fe59e4756c4379e6df8ba0cce0e04",
+    "url": "/chart_cv/index.html"
+  },
+  {
+    "revision": "80596bae41069b4a2c4d",
+    "url": "/chart_cv/static/css/main.36472cb0.chunk.css"
+  },
+  {
+    "revision": "eb26f52694f44d73956e",
+    "url": "/chart_cv/static/js/2.c9cd8f4b.chunk.js"
+  },
+  {
+    "revision": "fb6d71085796588b665e9bcc04d93d26",
+    "url": "/chart_cv/static/js/2.c9cd8f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80596bae41069b4a2c4d",
+    "url": "/chart_cv/static/js/main.8dd3ab23.chunk.js"
+  },
+  {
+    "revision": "0f56a39a083863a5d8cf",
+    "url": "/chart_cv/static/js/runtime-main.b3f4c4bc.js"
+  }
+]);
