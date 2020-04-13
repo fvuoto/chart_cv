5f0fc7084a659a5f2a4297b48ca679fa7a564a2f
Franco Vuoto <vuoto@eng.it>
2020-04-13 19:34:26 +0200

    update

--- precache-manifest.b1270a3e41a6179084582703183fff2a.js
+++ precache-manifest.b1270a3e41a6179084582703183fff2a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "388bb8a2cea1f46b19df83a694bf968b",
+    "revision": "eaf8c9fe2f4f6d7bf06c62811654d869",
     "url": "/chart_cv/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/chart_cv/static/css/2.4af70dfe.chunk.css"
   },
   {
-    "revision": "fd8e149a8ac216796082",
+    "revision": "69067aa92659f6558342",
     "url": "/chart_cv/static/css/main.c8e5e196.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/chart_cv/static/js/2.ea194e77.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fd8e149a8ac216796082",
-    "url": "/chart_cv/static/js/main.d6b89d5f.chunk.js"
+    "revision": "69067aa92659f6558342",
+    "url": "/chart_cv/static/js/main.9e267900.chunk.js"
   },
   {
     "revision": "6bc8f9022766d1614d1f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chart_cv/precache-manifest.3e03aa3bc65c271338e98b2055957213.js"
+  "/chart_cv/precache-manifest.b1270a3e41a6179084582703183fff2a.js"
 );
 
 self.addEventListener('message', (event) => {
