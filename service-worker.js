25a68054c84c0c725850b0fe23f94323024ec310
Franco Vuoto <vuoto@eng.it>
2020-04-20 15:52:37 +0200

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chart_cv/precache-manifest.aba8a60392f74d108f23b3c012a4fefe.js"
+  "/chart_cv/precache-manifest.ca9399139b47c3d56d350ed876bc518a.js"
 );
 
 self.addEventListener('message', (event) => {
