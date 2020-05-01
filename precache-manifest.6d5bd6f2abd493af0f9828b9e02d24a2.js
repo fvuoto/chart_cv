1b05c804573d064dafc890cb33c963df6f6a7cc9
franco vuoto <franco.vuoto@gmail.com>
2020-05-01 18:14:59 +0200

    update

---
+++ precache-manifest.6d5bd6f2abd493af0f9828b9e02d24a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30bdf9762acc4694a2e1bae19b4e7fb",
+    "url": "/chart_cv/index.html"
+  },
+  {
+    "revision": "be4e56b3eeeb3b62b6b5",
+    "url": "/chart_cv/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "f5348b084424c8656daf",
+    "url": "/chart_cv/static/css/main.358a9f3f.chunk.css"
+  },
+  {
+    "revision": "be4e56b3eeeb3b62b6b5",
+    "url": "/chart_cv/static/js/2.bb3e2bad.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/chart_cv/static/js/2.bb3e2bad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5348b084424c8656daf",
+    "url": "/chart_cv/static/js/main.a5d6cc8c.chunk.js"
+  },
+  {
+    "revision": "6bc8f9022766d1614d1f",
+    "url": "/chart_cv/static/js/runtime-main.9439c9bc.js"
+  }
+]);
